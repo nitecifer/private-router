8bd33e45224b421101e7c3e2da37d9ff5bd4bc02
codecraft26 <codescraft26@gmail.com>
2022-10-03 00:08:53 +0530

    making privte routes into it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-function App() {
+import React from 'react'
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import { Home } from './Home.jsx'
+import { Login } from './Login.jsx'
+import { Profile } from './Profile.jsx'
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <>
+   <Router>
+
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path ="/login" element={<Login/>}/>
+      <Route path ="/Account" element={<Profile/>}/>
+
+
+
+    </Routes>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
